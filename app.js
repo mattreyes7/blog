fa8ef75aad35d87f20164432bb404ce2688a9258
mattreyes7 <mattreyes615@gmail.com>
2017-12-10 00:47:58 -0800

    created index route and demo post

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // app config
-mongoose.connect("mongodb://localhost/blog", { useMongoClient: true });
+mongoose.connect('mongodb://localhost/blog', { useMongoClient: true });
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,10 +17,23 @@ var blogSchema = new mongoose.Schema({
   created: {type: Date, default: Date.now}
 });
 
-var Blog = mongoose.model("Blog", blogSchema);
+var Blog = mongoose.model('Blog', blogSchema);
+
 
 // RESTful ROUTES
 
+app.get('/', function(req, res){
+  res.redirect('/blogs')
+})
+app.get('/blogs', function(req, res){
+  Blog.find({}, function(err, blogs){
+    if(err){
+      console.log(err);
+    } else {
+      res.render('index', {blogs: blogs})
+    }
+  });
+});
 
 app.listen(3000, function(){
   console.log('listening');
