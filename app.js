8509a4b2fee6af4d29d6008c07160a0f059427c2
mattreyes7 <mattreyes615@gmail.com>
2017-12-18 15:39:13 -0800

    show route added

--- app.js
+++ app.js
@@ -50,8 +50,21 @@ app.post('/blogs', function(req, res){
       // redirect
       res.redirect('/blogs');
     }
-  })
-})
-app.listen(3000, function(){
-  console.log('listening');
+  });
 });
+
+// SHOW Route
+app.get('/blogs/:id', function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if(err){
+      res.redirect('/blogs');
+    } else {
+      res.render('show', {blog: foundBlog});
+      }
+    });
+  });
+
+
+  app.listen(3000, function(){
+    console.log('listening');
+  });
